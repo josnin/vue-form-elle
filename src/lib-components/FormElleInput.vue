56e0a09e40ccacb1ae283eb0f6005630474e4cca
josnin <nino.capati@gmail.com>
2021-04-28 20:47:24 +0800

    created form hooks

--- src/lib-components/FormElleInput.vue
+++ src/lib-components/FormElleInput.vue
@@ -27,94 +27,13 @@
 </template>
 
 <script lang="ts">
-import { inject, defineComponent, PropType, watch, computed, toRefs, ref } from 'vue';
-export default /*#__PURE__*/defineComponent({
+import { defineComponent } from 'vue';
+import { useForm, props1 } '@/use/useForm';
+export default defineComponent({
   name: "FormElleInput",
-  props: {
-      modelValue: {
-        type: [String, Number] as PropType<string|number>
-      },
-      id: {
-        type: String as PropType<string>,
-        required: true,
-      },
-      label: {
-        type: String as PropType<string>
-      },
-      help: {
-        type: String as PropType<string>,
-        required: false
-      },
-      selectClass: {
-        type: String as PropType<string>
-      },
-      labelClass: {
-        type: String as PropType<string>
-      },
-      inputClass: {
-        type: String as PropType<string>
-      },
-      divClass: {
-        type: String as PropType<string>
-      },
-      validationClass: {
-        type: String as PropType<string>
-      },
-      helpClass: {
-        type: String as PropType<string>
-      },
-      validationCustom: {
-        type: Object as PropType<any>
-      },
-      validationMessage: {
-        type: Object as PropType<any>
-      }
-  }
+  props: props1,
   setup(props: any) {
-    const frm = inject<any>('__frmMain');
-    let valMsg = ref<any>('');
-    const styling = computed(() => frm.styling)
-    const { modelValue } = toRefs(props)
-
-    const setCustomRulesAndValMsg = (newVal: any): void => {
-      // only apply for individual custom validation msg
-      if (props.validationCustom) {
-        for (const [k, v] of Object.entries(props.validationMessage)) {
-          if (props.validationCustom[k](newVal)) {
-            valMsg.value = v
-          }
-        }
-      }
-    }
-
-    const setValMsg = (): void => {
-      valMsg.value = '';
-      if (props.validationMessage && !props.validationCustom) {
-        Array.prototype.forEach.call(frm.frm._value.elements, function(element) {
-          for (const [k, v] of Object.entries(props.validationMessage)) {
-            if (element.validity[k]) {
-              //console.log(element.validity[k], k, v)
-              valMsg.value = v
-              return;
-            }
-          }
-        })
-      }
-      if (!valMsg.value) {
-        //console.log(props.id, frm.validity.value[props.id], props.modelValue)
-        valMsg.value = frm.validity.value[props.id];
-      }
-    }
-
-    watch(frm.validity, (): void => {
-      setValMsg()
-    }) 
-
-    watch(modelValue, (newVal: string): void => {
-      setCustomRulesAndValMsg(newVal);
-    })
-
-
+    const { valMsg, styling } = useForm(props);
     return {
       valMsg,
       styling
---
+++ src/use/useForm.ts
@@ -0,0 +1,96 @@
+//import { PropType, watch, onMounted, inject,  toRefs, ref, computed } from 'vue'
+import { inject, PropType, watch, computed, toRefs, ref } from 'vue';
+
+export function useForm(props: any) {
+  const frm = inject<any>('__frmMain');
+  let valMsg = ref<any>('');
+  const styling = computed(() => frm.styling)
+  const { modelValue } = toRefs(props)
+
+  const setCustomRulesAndValMsg = (newVal: any): void => {
+    // only apply for individual custom validation msg
+    if (props.validationCustom) {
+      for (const [k, v] of Object.entries(props.validationMessage)) {
+        if (props.validationCustom[k](newVal)) {
+          valMsg.value = v
+        }
+      }
+    }
+  }
+
+  const setValMsg = (): void => {
+    valMsg.value = '';
+    if (props.validationMessage && !props.validationCustom) {
+      Array.prototype.forEach.call(frm.frm._value.elements, function(element) {
+        for (const [k, v] of Object.entries(props.validationMessage)) {
+          if (element.validity[k]) {
+            //console.log(element.validity[k], k, v)
+            valMsg.value = v
+            return;
+          }
+        }
+      })
+    }
+    if (!valMsg.value) {
+      //console.log(props.id, frm.validity.value[props.id], props.modelValue)
+      valMsg.value = frm.validity.value[props.id];
+    }
+  }
+
+  watch(frm.validity, (): void => {
+    setValMsg()
+  }) 
+
+  watch(modelValue, (newVal: string): void => {
+    setCustomRulesAndValMsg(newVal);
+  })
+
+  return {
+    valMsg,
+    styling
+  }
+
+}
+
+export const props1 = {
+  modelValue: {
+    type: [String, Number] as PropType<string|number>
+  },
+  id: {
+    type: String as PropType<string>,
+    required: true,
+  },
+  label: {
+    type: String as PropType<string>
+  },
+  help: {
+    type: String as PropType<string>,
+    required: false
+  },
+  selectClass: {
+    type: String as PropType<string>
+  },
+  labelClass: {
+    type: String as PropType<string>
+  },
+  inputClass: {
+    type: String as PropType<string>
+  },
+  divClass: {
+    type: String as PropType<string>
+  },
+  validationClass: {
+    type: String as PropType<string>
+  },
+  helpClass: {
+    type: String as PropType<string>
+  },
+  validationCustom: {
+    type: Object as PropType<any>
+  },
+  validationMessage: {
+    type: Object as PropType<any>
+  }
+}
+
+  export default { props1, useForm }
